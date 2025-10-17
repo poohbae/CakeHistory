c2abfa7d5c6493d1111addf117733768a6e17792
poohbae <ameliadavid5687@yahoo.com>
2025-10-18 02:33:13 +0800

    update

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def load_user(user_id):
 # --------------------------
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('menu.html')
 
 @app.route('/menu')
 def menu():
