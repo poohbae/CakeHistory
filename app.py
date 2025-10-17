b383f017824c6ed3eb893ef635f411e3f981574f
poohbae <ameliadavid5687@yahoo.com>
2025-10-18 02:30:01 +0800

    update

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
 
 # Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = (
